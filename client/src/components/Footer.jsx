b357cd287e9ecc27c478ef870ad9d8b41ca85d83
Risto Tõldsep <r.toldsep@gmail.com>
2022-09-29 00:45:51 +0300

    Upgraded home page view

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import Logo from "../img/logo.png";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <img src={Logo} alt="" />
+      <span>
+        Made with ♥️ by{" "}
+        <a href="https://ristotoldsep.eu" target="_blank" rel="noreferrer" class="link">
+          @ristotoldsep
+        </a> using 
+        <b> React.js</b>.
+      </span>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
