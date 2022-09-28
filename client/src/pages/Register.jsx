aa34088d69821f5c310fce57897deff8dea1f2ed
Risto Tõldsep <r.toldsep@gmail.com>
2022-09-29 02:37:03 +0300

    Upgraded front end and created backend + connected with front (registration only)

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,21 +1,59 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Register = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [err, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("/auth/register", inputs);
+      navigate("/login");
+    } catch (err) {
+      setError(err.response.data);
+    }
+  };
+
   return (
     <div className="auth">
       <h1>Register</h1>
       <form>
-        <input required type="text" placeholder="username" name="username" />
-        <input required type="email" placeholder="email" name="email" />
+        <input
+          required
+          type="text"
+          placeholder="username"
+          name="username"
+          onChange={handleChange}
+        />
+        <input
+          required
+          type="email"
+          placeholder="email"
+          name="email"
+          onChange={handleChange}
+        />
         <input
           required
           type="password"
           placeholder="password"
           name="password"
+          onChange={handleChange}
         />
-        <button>Register</button>
-        {/* {err && <p>{err}</p>} */}
+        <button onClick={handleSubmit}>Register</button>
+        {err && <p>{err}</p>}
         <span>
           Do you have an account? <Link to="/login">Login</Link>
         </span>
