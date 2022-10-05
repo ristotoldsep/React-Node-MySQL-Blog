ff5c2230884eca00d79605f60b2b3e16a76cceb6
Risto Tõldsep <r.toldsep@gmail.com>
2022-10-05 19:58:51 +0300

    Updates

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,35 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import axios from "axios";
+import React, { useState } from "react";
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/authContext";
 
 const Login = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    password: "",
+  });
+  const [err, setError] = useState(null);
 
+  const navigate = useNavigate();
 
+  const { login, currentUser } = useContext(AuthContext);
+
+  console.log(currentUser);
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await login(inputs);
+      navigate("/");
+    } catch (err) {
+      setError(err.response.data);
+    }
+  };
   return (
     <div className="auth">
       <h1>Login</h1>
@@ -13,15 +39,17 @@ const Login = () => {
           type="text"
           placeholder="username"
           name="username"
+          onChange={handleChange}
         />
         <input
           required
           type="password"
           placeholder="password"
           name="password"
+          onChange={handleChange}
         />
-        <button>Login</button>
-        {/* {err && <p>{err}</p>} */}
+        <button onClick={handleSubmit}>Login</button>
+        {err && <p>{err}</p>}
         <span>
           Don't you have an account? <Link to="/register">Register</Link>
         </span>
